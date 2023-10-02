ee90e35f005ffb9ad887f883588306e55a1a9f7c
aki613 <107912112+aki613@users.noreply.github.com>
2023-10-02 14:25:12 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 // JavaScript
+
+
 console.log('Hello world!');
 
 console.log(document.getElementById('myColor').value);
 document.getElementById('bodyText').textContent = '文字の置き換え';
 document.getElementById('myColor').value;
 
+text.textContent = color.value;
+
 const color = document.getElementById('myColor');
 const text = document.getElementById('bodyText');
+
+function changeColor(){
+    // カラーコードを表示
+      text.textContent = color.value;
+    }
+    
+    // 背景色を変更
+  document.body.style.backgroundColor = color.value;
+
+
+// カラーピッカーで色が選択されたら、changeColorを動作させる
+color.addEventListener('input', changeColor);
+
 
 text.textContent = color.value;
